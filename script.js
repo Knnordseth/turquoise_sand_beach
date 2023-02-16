adc0a38d334e2382843c7456eb0f551c659ec2b8
Kristian <bollepusdodgebolle@gmail.com>
2023-02-16 21:43:49 +0100

    shit may not work nomore
    flytta hele script taggen fra html til script.js osv

--- script.js
+++ script.js
@@ -5,6 +5,43 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
+document.addEventListener('DOMContentLoaded', function() {
+  const loadEl = document.querySelector('#load');
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.firestore().doc('/foo/bar').get().then(() => { });
+  // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  // firebase.analytics(); // call to activate
+  // firebase.analytics().logEvent('tutorial_completed');
+  // firebase.performance(); // call to activate
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    let app = firebase.app();
+    let features = [
+      'auth', 
+      'database', 
+      'firestore',
+      'functions',
+      'messaging', 
+      'storage', 
+      'analytics', 
+      'remoteConfig',
+      'performance',
+    ].filter(feature => typeof app[feature] === 'function');
+    loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+  } catch (e) {
+    console.error(e);
+    loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+  }
+});
+
 const firebaseConfig = {
     apiKey: "AIzaSyCrCsxxIIYTUpF8uJ12a49UTsDx9KIrbtU",
     authDomain: "white-sand-beach.firebaseapp.com",
@@ -21,7 +58,6 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-
 
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
